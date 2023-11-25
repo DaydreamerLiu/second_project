33bef826755cb83ee786c5f0c8991241c9e0e66b
unprecedent <151455792+unprecedent@users.noreply.github.com>
2023-11-25 21:55:16 +0800

    Add files via upload

--- 关卡选项.cpp
+++ 关卡选项.cpp
@@ -1,3 +1,9 @@
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+#include"tools.h"
+#define SCREEN_WIDTH 900
+#define SCREEN_HIGH 600
 void Levle_choose_view()
 {
 	IMAGE mag1;//选关界面
@@ -5,8 +11,9 @@ void Levle_choose_view()
 	loadimage(&mag1, "图片/选关界面.jpg", SCREEN_WIDTH, SCREEN_HIGH);
 	loadimage(&ima1, "图片/按钮1.png", 90, 30);//鼠标没点时的 
 	loadimage(&ima2, "图片/按钮2.png", 90, 30);//鼠标点击时 
-	loadimage(&ima3, "图片/返回键.png", 200, 40);//鼠标没点时的 
-	loadimage(&ima4, "图片/返回键.png", 200, 40);//鼠标点击时 
+	loadimage(&ima3, "图片/返回键1.png", 200, 40);//鼠标没点时的 
+	loadimage(&ima4, "图片/返回键2.png", 200, 40);//鼠标点击时 
+	
 	int flag = 1;//设置点击时的图片细微变化 
 	while (1)
 	{
@@ -18,6 +25,20 @@ void Levle_choose_view()
 		putimagePNG(550, 300, flag ? &ima1 : &ima2 );
 		putimagePNG(700, 300, flag ? &ima1 : &ima2 );
 		putimagePNG(350, 515, flag ? &ima3 : &ima4 );
+		settextstyle(15, 10, "黑体");
+		setbkmode(TRANSPARENT);
+		settextcolor(RGB(30, 30, 30));
+		outtextxy(115, 307, "关卡一");
+		outtextxy(265, 307, "关卡二");
+		outtextxy(415, 307, "关卡三");
+		outtextxy(565, 307, "关卡四");
+		outtextxy(715, 307, "关卡五");
+		outtextxy(400, 527, "返回主菜单");
+		settextstyle(38, 40, "黑体");
+		setbkmode(TRANSPARENT);
+		outtextxy(285, 32, "关卡选项");
+		EndBatchDraw();
+		/*
 		ExMessage m;
 		if (peekmessage(&m, EX_MOUSE))//关卡1 
 		{
@@ -108,8 +129,15 @@ void Levle_choose_view()
 
 			}
 
-		}
-        EndBatchDraw();
+		}*/
+        
 	}
+}
+int main()
+{
+	initgraph(900, 600);
+	Levle_choose_view();
+	getchar();
+	return 0;
 }
 
